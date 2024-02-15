1ba3625d66cce48b27c4c6ba03a9f336e03719ba
yk0611 <hello@junkbranding.com>
2024-02-15 17:41:59 +0900

    first commit

---
+++ app/blog/page.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+async function getAllBlogs () {
+  const files = fs.readdirSync(path.join('data'));
+  const blogs = files.map(filename => {
+    const fileData = fs.readFileSync (
+      path.join(process.cwd(), 'data', filename),
+      'utf-8'
+    );
+    const { data } = matter(fileData);
+    return {
+      frontmatter: data,
+    }
+  })
+  return {
+    blogs: blogs
+  }
+}
+
+const Blog = async() => {
+  const { blogs } = await getAllBlogs();
+  console.log(blogs);
+  return (
+    <div>
+      <h1>Blog</h1>
+
+        {blogs.map((blog,index) => 
+          <div key={index}>
+            <h2>{blog.frontmatter.title}</h2>
+            <p>{blog.frontmatter.date}</p>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default Blog;
